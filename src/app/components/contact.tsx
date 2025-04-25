97c0a0df9d9daebbf73ea690da1b8e2e3c4ed7db
ezzeldeen tantawy <eldeene103@gmail.com>
2025-04-25 17:56:49 +0300

    add the api

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -15,7 +15,7 @@ export const Contact: React.FC = () => {
     const [success, setSuccess] = useState(false);
 
     const sendContactForm = async (formData: FormData) => {
-        const response = await fetch(`http://127.0.0.1:8000/api/contact`, {
+        const response = await fetch(`https://my0new-portfolio-backend-production.up.railway.app/api/contact`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(formData),
